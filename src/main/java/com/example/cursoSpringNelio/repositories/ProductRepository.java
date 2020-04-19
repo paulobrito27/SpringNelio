98c1803400b52cc876a37c59802773c8ad31a17a
paulobrito27 <paulojmbrito27@gmail.com>
2020-04-19 16:48:54 -0300

    Aula que a claudia assistiu - product

---
+++ src/main/java/com/example/cursoSpringNelio/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.example.cursoSpringNelio.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.cursoSpringNelio.entities.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
